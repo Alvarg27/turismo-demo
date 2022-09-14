c168731dd67402edbcc9a00782dba951a51b0e8f
Alvarg27 <alvargbesne@gmail.com>
2022-09-14 17:04:04 -0500

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 
 const Banner = () => {
   return (
-    <div className="h-[100px] w-full relative bg-indigo-700 mb-10">
+    <div className="h-[100px] w-full relative bg-indigo-700 z-[50] mb-10">
       <SearchBar className="absolute -bottom-[25px] left-1/2 -translate-x-1/2 px-4" />
     </div>
   );
