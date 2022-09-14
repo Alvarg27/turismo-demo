8081945e0c87933f87904fb22becd143c912dadb
Alvarg27 <alvargbesne@gmail.com>
2022-09-14 17:39:32 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="">
       <Banner />
-      <div className="px-8">
+      <div className="px-4">
         <Recommended />
       </div>
     </div>
