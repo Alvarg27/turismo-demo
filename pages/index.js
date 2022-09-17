49484501cbb4730f0d4ee87f01c652d70f05c0cb
Alvarg27 <alvargbesne@gmail.com>
2022-09-17 16:34:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,10 @@ export default function Home() {
   return (
     <>
       <div className="">
+        <Categories />
         <VideoCard />
         <Nearby />
-        <Categories />
+
         <Recommended />
       </div>
     </>
