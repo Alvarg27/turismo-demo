9859bac308db25f1d888ca35d9ede2dfcc739ede
Alvarg27 <alvargbesne@gmail.com>
2022-09-21 01:34:49 -0500

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,17 +5,20 @@ import SearchBar from "./SearchBar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="h-[1000px]  bg-white relative">
-      <div className="flex h-[50px] bg-indigo-700">
-        <div className="m-auto text-white flex mt-[15px]">
-          <FaPlane className="m-auto text-4xl mr-2" />
-          <p className="my-auto text-xl">Turismo</p>
+    <div className="bg-white">
+      <div className="sticky top-0 z-[40] mb-[75px]">
+        <div className="flex h-[50px] bg-blue-700">
+          <div className="m-auto text-white flex mt-[15px]">
+            <FaPlane className="m-auto text-4xl mr-2" />
+            <p className="my-auto text-xl">Turismo</p>
+          </div>
+        </div>
+
+        <div className="w-full bg-blue-700 z-[50] mb-10 h-[40px] absolute">
+          <SearchBar className="absolute -bottom-[25px] left-1/2 -translate-x-1/2 px-4" />
         </div>
       </div>
 
-      <div className="w-full bg-indigo-700 z-[50] mb-10 h-[40px] sticky top-0">
-        <SearchBar className="absolute -bottom-[25px] left-1/2 -translate-x-1/2 px-4" />
-      </div>
       {children}
     </div>
   );
